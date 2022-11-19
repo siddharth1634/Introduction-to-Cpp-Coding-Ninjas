1dd9ebb4a7256b73a6f0fa32a4e49c68e8042d7b
siddharth1634 <112333855+siddharth1634@users.noreply.github.com>
2022-11-19 14:14:11 +0530

    Create All Prime Numbers.cpp

---
+++ Operation and For Loop/All Prime Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+    cin>>n;
+    
+	for(int i=2; i<=n; i++)
+    {
+        int temp=0;
+    	for(int j=2; j<i; j++)
+    	{
+        	if(i%j==0)
+        	{
+            	temp++;
+        	}
+    	}
+        if(temp==0)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
